8970573a5a6758d82e4339b9966ae9121377a858
Debapriya Sengupta <95506839+DebapriyaSengupta28@users.noreply.github.com>
2024-01-15 00:38:50 +0530

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var sayHiButton = document.getElementById('sayHiButton');
+    sayHiButton.addEventListener('click', function() {
+        alert("Hi");
+    }, false);
+}, false);
